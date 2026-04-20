a7ab5d5f8d59aa3388a64d21adf4ac32a47b041e
泽希安 <117739556+zxiar@users.noreply.github.com>
2026-04-20 23:57:59 +0800

    Add files via upload

--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,15 @@ export interface GroupWithSites extends Omit<Group, 'id'> {
   id: number; // 确保id始终存在
   sites: Site[];
 }
+
+export interface BeforeInstallPromptEvent extends Event {
+  readonly platforms: string[];
+  readonly userChoice: Promise<{ outcome: 'accepted' | 'dismissed'; platform: string }>;
+  prompt(): Promise<void>;
+}
+
+declare global {
+  interface WindowEventMap {
+    beforeinstallprompt: BeforeInstallPromptEvent;
+  }
+}
