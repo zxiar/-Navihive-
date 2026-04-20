e061ad036bc864e5819ac4a315fd2389a4797a95
泽希安 <117739556+zxiar@users.noreply.github.com>
2026-04-21 00:18:29 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-import { cloudflare } from "@cloudflare/vite-plugin";
+import { cloudflare } from '@cloudflare/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), cloudflare()],
-})
+  server: {
+    host: true, // 允许通过 IP 地址访问
+    port: 5173, // 默认端口
+  },
+});
